0be04ddcc7ade8add3882a63093755e03b90b99e
sabohat <sabohat2802@gmail.com>
2021-07-11 15:07:32 +0500

    add: sockey connection functions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const socket = require('socket.io')
 const app = express()
 
 const server = app.listen(4000, () => {
-  
+
   console.log('listening to port 4000')
 })
 
@@ -15,4 +15,13 @@ const io = socket(server)
 
 io.on('connection', (socket) => {
   console.log('connected to socket')
+
+  socket.on('chat', (data)=> {
+    io.sockets.emit('chat', data)
+  })
+
+  socket.on('typing', (data)=> {
+    socket.broadcast.emit('typing', data)
+  })
 })
+
